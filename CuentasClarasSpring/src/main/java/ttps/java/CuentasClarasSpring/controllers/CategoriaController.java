32efe7e85b8018556e803c0119f33b411e9c1219
Manaaa22 <keimmariana@gmail.com>
2023-12-16 14:24:25 -0300

    Se agrego @CrossOrigin y dependencia @JsonIgnore

--- CuentasClarasSpring/src/main/java/ttps/java/CuentasClarasSpring/controllers/CategoriaController.java
+++ CuentasClarasSpring/src/main/java/ttps/java/CuentasClarasSpring/controllers/CategoriaController.java
@@ -3,6 +3,7 @@ package ttps.java.CuentasClarasSpring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ttps.java.CuentasClarasSpring.model.Categoria;
 import ttps.java.CuentasClarasSpring.services.CategoriaService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
